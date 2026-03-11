09c2c38e0f4823f634f31eb7e81c7c988798fb8b
vegedy <benito@benitozenz.de>
2026-03-11 07:35:52 +0100

    Download the dataset

---
+++ main.py
@@ -0,0 +1,16 @@
+import kagglehub
+import os
+
+# Download the dataset
+path = kagglehub.dataset_download("mostafaabla/garbage-classification")
+print("Path to dataset files:", path)
+
+# List all directories in the dataset and count the total number of files
+file_count = 0
+for root, dirs, files in os.walk(path):
+    if len(dirs) > 1:
+        print("Classes:", dirs)
+    for file in files:
+        file_count += 1
+
+print("Total files:", file_count)
